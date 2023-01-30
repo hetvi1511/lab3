9889f5eacb5b100d79d4eebde6650703558acc27
hetvi1511 <hgandhi@ucsd.edu>
2023-01-29 19:36:11 -0800

    .

--- ListTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-
-import java.util.List;
-public class ListTests {
-    @Test
-    public void filterTest(){
-        List<String> input1 = new List<String>("what","are","you","doing");
-    }
-}
